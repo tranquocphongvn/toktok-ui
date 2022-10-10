7169c45baf81c27ecc1a599c799aa4c880de8953
Phong Tran <tranquocphongvn@gmail.com>
2022-10-10 00:41:40 -0700

    DefaultLayout

--- src/components/Layouts/DefaultLayout/Sidebar/index.js
+++ src/components/Layouts/DefaultLayout/Sidebar/index.js
@@ -1,6 +1,14 @@
+import classNames from 'classnames/bind';
+
+import styles from './Sidebar.module.scss';
+
+const cx = classNames.bind(styles);
+
 function Sidebar() {
     return ( 
-        <h2>Sidebar</h2>
+        <aside className={cx('wrapper')}>
+            <h3>Sidebar</h3>
+        </aside>
     );
 }
 
--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,6 +1,17 @@
+import classNames from 'classnames/bind';
+
+import styles from './Header.module.scss';
+
+const cx = classNames.bind(styles);
+
 function Header() {
     return ( 
-        <h2>Header</h2>
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <h3>Header</h3>
+                {/* logo, search box... */}
+            </div>
+        </header>
     );
 }
 
